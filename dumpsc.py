3f32005e34f3dae48568294b6656b3b7f37dcd64
123456abcdef <rubish@web.de>
2016-05-07 21:26:51 +0200

    Multiple texture support per file
    Thx to knobse for the hint!
    subType 6 and 10 pixels coding still needs to be figured out.

--- dumpsc.py
+++ dumpsc.py
@@ -23,6 +23,16 @@ def convert_pixel(pixel, type):
     elif type == 4:
         pixel, = struct.unpack("<H", pixel)
         return (((pixel >> 11) & 0x1F) << 3, ((pixel >> 5) & 0x3F) << 2, (pixel & 0x1F) << 3)
+    elif type == 6:
+        pixel, = struct.unpack("<H", pixel)
+        # TODO figure out correct colors
+        return (((pixel >> 12) & 0xF) << 4, ((pixel >> 8) & 0xF) << 4,
+                ((pixel >> 4) & 0xF) << 4, ((pixel >> 0) & 0xF) << 4)
+    elif type == 10:
+        pixel, = struct.unpack("<B", pixel)
+        # TODO figure out correct colors
+        return (((pixel >> 12) & 0xF) << 4, ((pixel >> 8) & 0xF) << 4,
+                ((pixel >> 4) & 0xF) << 4, ((pixel >> 0) & 0xF) << 4)
     else:
         raise Exception("Unknown pixel type {}".format(type))
 
@@ -32,32 +42,47 @@ def process_sc(baseName, data, path):
     data = data[0:9] + ('\x00' * 4) + data[9:]
     decompressed = lzma.LZMADecompressor().decompress(data)
 
-    fileType, = struct.unpack('<b', decompressed[0])
-    # fileSize = decompressed[1:5][::-1]
-    subType, = struct.unpack('<b', decompressed[5])
-    width, = struct.unpack('<H', decompressed[6:8])
-    height, = struct.unpack('<H', decompressed[8:10])
+    i = 0
+    picCount = 0
+    while len(decompressed[i:]) > 5:
+        fileType, = struct.unpack('<b', decompressed[i])
+        fileSize, = struct.unpack('<I', decompressed[i + 1:i + 5])
+        subType, = struct.unpack('<b', decompressed[i + 5])
+        width, = struct.unpack('<H', decompressed[i + 6:i + 8])
+        height, = struct.unpack('<H', decompressed[i + 8:i + 10])
+        i += 10
 
-    i = 10
-    if subType == 2 or subType == 4:
-        pixelSize = 2
-    elif subType == 0:
-        pixelSize = 4
-    else:
-        raise Exception("Unknown pixel type {}".format(subType))
+        if subType == 0:
+            pixelSize = 4
+        elif subType == 2 or subType == 4 or subType == 6:
+            pixelSize = 2
+        elif subType == 10:
+            pixelSize = 1
+        else:
+            raise Exception("Unknown pixel type {}".format(subType))
 
-    img = Image.new("RGBA", (width, height))
-    pixels = []
-    for y in range(height):
-        for x in range(width):
-            pixels.append(convert_pixel(decompressed[i:i + pixelSize], subType))
-            i += pixelSize
-    img.putdata(pixels)
+        print('fileType: {}, fileSize: {}, subType: {}, width: {}, height: {}'.format(fileType,
+                                                                                      fileSize,
+                                                                                      subType,
+                                                                                      width,
+                                                                                      height))
 
-    img.save(path + baseName + '.png', 'PNG')
+        img = Image.new("RGBA", (width, height))
+        pixels = []
+        for y in range(height):
+            for x in range(width):
+                pixels.append(convert_pixel(
+                    decompressed[i:i + pixelSize], subType))
+                i += pixelSize
+
+        img.putdata(pixels)
+        img.save(path + baseName + ('_' * picCount) + '.png', 'PNG')
+
+        picCount += 1
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Extract png files from Clash Royale "*_tex.sc" files')
+    parser = argparse.ArgumentParser(
+        description='Extract png files from Clash Royale "*_tex.sc" files')
     parser.add_argument('files', help='sc file', nargs='+')
     parser.add_argument('-o', help='Extract pngs to directory', type=str)
     args = parser.parse_args()
